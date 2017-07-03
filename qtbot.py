3f18d6c6e352be98e6d0d9c67d1657804373507b
James <naughtsee@gmail.com>
2017-07-03 12:01:52 -0400

    minor updates

--- qtbot.py
+++ qtbot.py
@@ -31,8 +31,6 @@ startTime = datetime.now()
 startTimeStr = startTime.strftime("%B %d %H:%M:%S")
 
 # Basic information printed via stdout
-
-
 @bot.event
 async def on_ready():
     print("Client logged in at {}".format(startTimeStr))
