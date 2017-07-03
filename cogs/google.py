c8a3b4c536a8cc94d16dc84466c72d6089000272
James <naughtsee@gmail.com>
2017-07-03 15:16:58 -0400

    added my own google function

--- cogs/google.py
+++ cogs/google.py
@@ -10,14 +10,19 @@ class Google():
     def __init__(self, bot):
         self.bot = bot
 
+    # Scrapable search URL
     uri = "http://google.com/search?q={}"
+
+    # Rotate between a few different chromium headers
+    # Maybe this will help google to not block me (:
     headers = [{"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}, {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"}, {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"}]
   
     @commands.bot.command(name="google", aliases=["g", "goog"])
     async def new_google(self, *args):
         """ Get the first Google result for a query """
 
-        requests_cache.install_cache(expire_after=86400)
+        # Cache results for a half hour
+        requests_cache.install_cache(expire_after=1800)
 
         # Queries must be '+' delimited
         query = "+".join(args)
@@ -28,9 +33,14 @@ class Google():
 
         # Create google search URL
         query_url = self.uri.format(query)
+
+        # Gather webpage
         result = requests.get(query_url, headers=self.headers[random.randint(0, len(self.headers))]).text
+
+        # Extract links from page
         link_list = gf.get_google_links(result)
 
+        # Say first result
         return await self.bot.say("{}".format(link_list[0]))
 
 def setup(bot):
--- cogs/utils.py
+++ cogs/utils.py
@@ -119,17 +119,22 @@ class DictManip():
 
 class GoogleFuncs():
     def get_google_links(html):
+        """ Uses bs4 to extract links from google """
+        # Note, this only works with chromium headers
+
         soup = BeautifulSoup(html, "html.parser")
         links = []
 
+        # Finds the links
         for div in soup.findAll('h3', {'class': 'r'}):
             a = div.find('a')
             link = a.attrs['href']
             links.append(link)
 
+        # Some links are self-referencing the google search
+        # Remove them
         for link in links:
             if link.startswith("/"):
                 links.remove(link)
 
         return links
-        
