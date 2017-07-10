404d7fb95fc370f36fc673788ce165da6264c897
James <naughtsee@gmail.com>
2017-07-10 19:20:38 -0400

    fixed typo and added comments

--- cogs/news.py
+++ cogs/news.py
@@ -20,14 +20,17 @@ class News():
     async def get_news(self, sort_by="top"):
         """ Get top, latest, or popular news from Google, powered by https://newsapi.org/ """
 
-        # All allowed
+        # All allowed sorting types
         sorting_list = ["top", "latest", "popular"]
 
+        # If sort type specified is not allowed
         if not sort_by.lower() in sorting_list:
-            return await self.bot.say("Sorry, please chose `.news` followed by `top` `latest` `popular` or nothing at all to get news.")
+            return await self.bot.say("Sorry, please use `.news` followed by `top` `latest` `popular` or nothing at all to get news.")
 
+        # Call to API
         raw_result = requests.get(uri.format(sort_by, news_api_key)).json()
 
+        # Hideous, but I can't think of a better way to do it (:
         return await self.bot.say("`{}`\n<{}>\n`{}`\n<{}>\n`{}`\n<{}>\n`{}`\n<{}>\n`{}`\n<{}>".format(raw_result["articles"][0]["title"], raw_result["articles"][0]["url"], raw_result["articles"][1]["title"], raw_result["articles"][1]["url"], raw_result["articles"][2]["title"], raw_result["articles"][2]["url"], raw_result["articles"][3]["title"], raw_result["articles"][3]["url"], raw_result["articles"][4]["title"], raw_result["articles"][4]["url"]))
 
 
