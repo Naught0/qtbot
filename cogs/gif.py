77c41451306b15234e6d444218ee3ef70e56b9cd
James <naughtsee@gmail.com>
2017-08-07 02:49:56 -0400

    fixed gif command

--- cogs/gif.py
+++ cogs/gif.py
@@ -7,13 +7,16 @@ class Giphy:
     def __init__(self, bot):
         self.bot = bot
 
-    def sync_giphy(self, query):
+    def sync_giphy(self, query=None):
         """ Non async giphy library function """
         giphy_instance = giphypop.Giphy()
 
+        if query is None:
+            return giphy_instance.random_gif()
+
         gif = giphy_instance.random_gif(query)
 
-        if gif:
+        if gif: 
             return gif
 
         return gif
@@ -23,13 +26,12 @@ class Giphy:
     async def giphy(self, ctx, *args):
         """ returns a random gif matching a query """
         q = " ".join(args)
-
         gif_result = await self.bot.loop.run_in_executor(None, Giphy.sync_giphy, q)
 
         if gif_result:
-            ctx.send(gif_result.media_url)
+            await ctx.send(gif_result.media_url)
         else:
-            ctx.send("Sorry, I couldn't find anything for `{}`.".format(q))
+            await ctx.send("Sorry, I couldn't find anything for `{}`.".format(q))
 
     # Thanks qtbot!
     @commands.command(aliases=['ty', 'thank'])
@@ -37,7 +39,7 @@ class Giphy:
         """ Thank your overlord, qtbot """
         gif_result = await self.bot.loop.run_in_executor(None, Giphy.sync_giphy, "blush")
 
-        await ctx.send(gif.fixed_width.downsampled.url)
+        await ctx.send(gif_result.fixed_width.downsampled.url)
 
 
 def setup(bot):
