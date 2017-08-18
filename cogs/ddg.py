e23dc50b96d1600c8fe8d57bc29c20acd9712b88
James <4589250+Naught0@users.noreply.github.com>
2017-08-18 11:02:48 -0400

    syntax error

--- cogs/ddg.py
+++ cogs/ddg.py
@@ -14,7 +14,7 @@ class DuckDuckGo:
     def __init__(self, bot):
         self.bot = bot
         self.base_uri = "http://api.duckduckgo.com/?q={}&format=json&t=qtbot-1.0-discord-bot"
-        self.headers = headers = [{"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}, {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"}, "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"}]
+        self.headers = headers = [{"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}, {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"}, {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"}]
 
     @commands.command(name="ddg", aliases=["dg"])
     async def duckduckgo_search(self, ctx, *, query):
