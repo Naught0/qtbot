85d3b1203d9861f986356e593a2b79d96c38c1b3
James <4589250+Naught0@users.noreply.github.com>
2017-08-18 01:22:29 -0400

    Revert "progress on DDG cog & aiohttp wrapper"
    This reverts commit 6b6d243e96bd13583e7f02dfe6669578d238a594.

--- utils/aiohttp_wrap.py
+++ utils/aiohttp_wrap.py
@@ -3,8 +3,9 @@
 import aiohttp
 
 
-async def aio_get_text(url, headers=None):
+async def aio_get(url: str):
     async with aiohttp.ClientSession() as session:
+<<<<<<< HEAD
         async with session.get(url, headers=headers) as r:
             if r.status == 200:
                 return r.text()
@@ -18,3 +19,10 @@ async def aio_get_json(url, headers=None):
                 return r.json()
             else:
                 return None
+=======
+    async with session.get(url) as r:
+        if r.status == 200:
+            return r
+        else:
+            return None
+>>>>>>> parent of 6b6d243... progress on DDG cog & aiohttp wrapper
