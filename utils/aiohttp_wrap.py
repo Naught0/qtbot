91028bb00cdad00b64783d57f4b7af5d8b3845b9
James <4589250+Naught0@users.noreply.github.com>
2017-08-18 11:26:15 -0400

    missing await in wrapper

--- utils/aiohttp_wrap.py
+++ utils/aiohttp_wrap.py
@@ -8,7 +8,7 @@ async def aio_get_text(url, headers=None):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as r:
             if r.status == 200:
-                return r.text()
+                return await r.text()
             else:
                 return None
 
@@ -16,6 +16,6 @@ async def aio_get_json(url, headers=None):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as r:
             if r.status == 200:
-                return r.json()
+                return await r.json()
             else:
                 return None
