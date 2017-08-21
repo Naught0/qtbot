bcad14f4ee79a43cc06ee9429b5073c3593ae84f
James <4589250+Naught0@users.noreply.github.com>
2017-08-21 15:18:27 -0400

    final fix

--- utils/aiohttp_wrap.py
+++ utils/aiohttp_wrap.py
@@ -2,10 +2,18 @@
 
 import aiohttp
 
-async def aio_get(aio_session, url, headers=None):
+async def aio_get_text(aio_session, url, headers=None):
     async with aio_session() as s:
         async with s.get(url, headers=headers) as r:
             if r.status == 200:
-                return await r
+                return await r.text()
+            else:
+                return None
+
+async def aio_get_json(aio_session, url, headers=None):
+    async with aio_session() as s:
+        async with s.get(url, headers=headers) as r:
+            if r.status == 200:
+                return await r.json()
             else:
                 return None
