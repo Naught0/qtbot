dc4c865bfc7fe85340c3d6656e5ed323fe976ad8
James <naughtsee@gmail.com>
2017-08-21 19:33:54 -0400

    isup.py moved to aiohttp

--- cogs/isup.py
+++ cogs/isup.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 
 import discord
-import requests
+from utils import aiohttp_wrap as aw
 from bs4 import BeautifulSoup
 from discord.ext import commands
 
@@ -10,17 +10,19 @@ class DownDetect:
     def __init__(self, bot):
         self.bot = bot
         self.uri = 'http://downforeveryoneorjustme.com/{}'
+        self.aio_session = bot.aio_session
 
     @commands.command(name='isup', aliases=['dd'])
+    @commands.cooldown(rate=1, per=2.0, type=commands.BucketType.user)
     async def down_detector(self, ctx, check_url):
         """ Check whether a website is down or up """
 
-        request_html = requests.get(self.uri.format(check_url)).text
+        request_html = await aw.aio_get_text(self.uri.format(check_url))
 
         # Get soupy
         soup = BeautifulSoup(request_html, 'html.parser')
 
-        # Ugly but ensures that a website with 'not'
+        # Ugly but ensures that a website with 'not' in the url
         # Is not erroneously detected
         isup_result = str(soup.find('p')).split('>')[1].strip().split('<')[0]
 
