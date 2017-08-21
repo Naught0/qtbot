cea34a9273ddb6bf168e0e9b48456ce1d1cb63a4
James <4589250+Naught0@users.noreply.github.com>
2017-08-21 15:06:44 -0400

    remove unecessary await

--- utils/aiohttp_wrap.py
+++ utils/aiohttp_wrap.py
@@ -6,6 +6,6 @@ async def aio_get(aio_session, url, headers=None):
     async with aio_session() as s:
         async with s.get(url, headers=headers) as r:
             if r.status == 200:
-                return await r
+                return r
             else:
                 return None
