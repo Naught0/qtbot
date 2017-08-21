061c84fd512c28934dac68d62e55122a1dda5ac8
James <4589250+Naught0@users.noreply.github.com>
2017-08-21 14:59:13 -0400

    eh?

--- utils/aiohttp_wrap.py
+++ utils/aiohttp_wrap.py
@@ -3,7 +3,7 @@
 import aiohttp
 
 async def aio_get(aio_session, url, headers=None):
-    async with aio_session as s:
+    async with aio_session() as s:
         async with s.get(url, headers=headers) as r:
             if r.status == 200:
                 return await r.text()
