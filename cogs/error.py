8f7ffab2d405e9103038d82c124ed34487bca454
James <4589250+Naught0@users.noreply.github.com>
2017-09-03 00:15:06 -0400

    fix fstring error in error.py

--- cogs/error.py
+++ cogs/error.py
@@ -20,7 +20,7 @@ class ErrorHandler:
             return await ctx.send(f'This command is on cooldown. Please retry in {error.retry_after} second(s).')
 
         if isinstance(error, discord.ext.commands.BotMissingPermissions):
-            return await ctx.send(f'I can\'t do that! I\'m missing permissions: ```{"\n".join(error.missing_perms)}```')
+            return await ctx.send(f'I can\'t do that! I\'m missing permissions: ```{error.missing_perms}```')
 
         if isinstance(error, discord.ext.commands.MissingRequiredArgument):
             return await ctx.send(f'Command missing required argument `{error.param}`')
