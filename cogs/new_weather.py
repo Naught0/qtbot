4798627f48430004d2bdaaf2aa3f7fbb89c3c06d
James <4589250+Naught0@users.noreply.github.com>
2017-09-04 16:39:20 -0400

    add new weather testing

---
+++ cogs/new_weather.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+
+import discord
+import json
+from utils import aiohttp_wrap as aw
+from utils import user_funcs as ufm
+from datetime import datetime
+from discord.ext import commands
+from aiocache import cached
+
+class Weather:
+    def __init__(self, bot):
+        self.bot = bot
+        self.aio_session = bot.aio_session
+        self.weather_icon_dict = {} # Empty for now
+        self.api_url = 'http://api.openweathermap.org/data/2.5/weather?zip={},{}'
+        self.icon_url = "http://openweathermap.org/img/w/{weather_data['weather'][0]['icon']}.png" # for later use
+
+    @commands.command(name='nu_weather', aliases=['nwt'])
+    @cached(ttl=1200)
+    async def get_weather(self, ctx, zip_code=None):
+        """ Get the weather via a new source """
+
+        # Initial start time
+        start_time = datetime.now()
+
+        resp = await aw.aio_get_json(self.aio_session, self.api_url.format('33073', 'us'))
+
+        now_time = datetime.now()
+
+        await ctx.send(f'Response:\n ```{resp}```\nTime: `{str(now_time - start_time)}`')
+
+
+def setup(bot):
+    bot.add_cog(Weather(bot))
+    
