9b75c5d9c625ad6f6bc6c48725a279516df2b02c
James <4589250+Naught0@users.noreply.github.com>
2017-09-07 03:47:42 -0400

    json fuckery

--- cogs/news.py
+++ cogs/news.py
@@ -53,7 +53,7 @@ class News:
         else:
             api_response = await aw.aio_get_json(self.aio_session, self.uri.format(self.api_key))
             article_list = api_response['articles']
-            await self.redis_client.set('news', json.dumps(article_list), ex=300)
+            await self.redis_client.set('news', json.dumps(api_response), ex=300)
 
             for article in article_list:
                 em_list.append(News.json_to_embed(article))
