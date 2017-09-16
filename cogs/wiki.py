2bc744eac9a826c47459f16780b8d0a9e8dced53
James <4589250+Naught0@users.noreply.github.com>
2017-09-15 22:46:00 -0400

    typo & small PEP8 change

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -14,7 +14,7 @@ class Wiki:
         self.aio_session = bot.aio_session
 
     @commands.command(name='wiki', aliases=['wi'])
-    async def wiki_seearch(self, ctx, *, query=None):
+    async def wiki_search(self, ctx, *, query=None):
         """ Get the closest matching Wikipedia article for a given query """
 
         # Determine whether we want a random article
@@ -30,7 +30,7 @@ class Wiki:
 
         # No result found
         if not wiki_info[1]:
-            return await ctx.send(f'Sorry, I couldn\'t find anything for `{query}`.')
+            return await ctx.send(f"Sorry, I couldn't find anything for `{query}`.")
 
         # Create embed
         em = discord.Embed()
