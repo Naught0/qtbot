4a0e140a023fcade597765aac7e3dea9c371ac70
James <naughtsee@gmail.com>
2017-09-18 22:12:19 -0400

    ａｅｓｔｈｅｔｉｃdocstring

--- cogs/generic.py
+++ cogs/generic.py
@@ -79,6 +79,7 @@ class Generic:
 
     @commands.command(aliases=['at'])
     async def aesthetify(self, ctx, *, a_text):
+        """ Make your message ａｅｓｔｈｅｔｉｃ，　ｍａｎ """
         ascii_to_wide = dict((i, chr(i + 0xfee0)) for i in range(0x21, 0x7f))
         ascii_to_wide.update({0x20: u'\u3000', 0x2D: u'\u2212'})
 
