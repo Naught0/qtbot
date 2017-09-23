cfd80daa51628c4c3d034a826227b53556a5a389
Mitchell Ferree <thebeatbutton@gmail.com>
2017-09-22 22:56:03 -0600

    book fixed ur code
    idk this probably works

--- cogs/ask.py
+++ cogs/ask.py
@@ -14,6 +14,7 @@ class AskAsk:
         self.scrape_uri = 'http://www.ask.com/web?q={}&o=0&qo=homepageSearchBox'
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
+    @staticmethod
     def _get_ask_links(html):
         """ Gets all result links from [REDACTED] """
         soup = BeautifulSoup(html, 'lxml')
@@ -24,13 +25,10 @@ class AskAsk:
             if not link['href'].startswith('//'):
                 link_list.append(link['href'])
 
-        if not link_list:
-            return None
-
-        return link_list
+        return link_list or None
 
     @commands.command(name='ask', aliases=['g'])
-    async def ask_search(self, ctx, *, query):
+    async def ask_search(self, ctx, *query):
         """ Get search results from [REDACTED], now that Google hates me. """
 
         # Handle no input
@@ -38,7 +36,7 @@ class AskAsk:
             return await ctx.send('Feel free to search something.')
 
         # Format query for search url
-        search_query = query.replace(' ', '+')
+        search_query = '+'.join(query)
 
         # Check if this has been searched in the cache
         if await self.redis_client.exists(f'ask:{search_query}'):
@@ -48,7 +46,7 @@ class AskAsk:
         # Actually request page html and store the link list for 6 hrs
         else:
             resp_html = await aw.aio_get_text(self.aio_session, self.scrape_uri.format(search_query), headers=self.headers)
-            link_list = AskAsk._get_ask_links(resp_html)
+            link_list = self._get_ask_links(resp_html)
 
             if link_list:
                 await self.redis_client.set(f'ask:{search_query}', f'{link_list}', ex=21600)
