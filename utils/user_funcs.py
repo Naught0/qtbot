519a4478674dbb0f0c72d2c31a43afc1632d058b
James <naughtsee@gmail.com>
2017-09-23 18:34:09 -0400

    naming consistency

--- utils/user_funcs.py
+++ utils/user_funcs.py
@@ -3,15 +3,15 @@
 import asyncpg
 
 class PGDB:
-    def __init__(self, db_conn):
-        self.db_conn = db_conn
+    def __init__(self, pg_con):
+        self.pg_con = pg_con
 
     async def fetch_user_info(self, member_id: int, column: str):
         query = f'''SELECT {column} FROM user_info WHERE member_id = {member_id};'''
-        return await self.db_conn.fetchval(query)
+        return await self.pg_con.fetchval(query)
 
     async def insert_user_info(self, member_id: int, column: str, col_value):
         execute = (
             f'''INSERT INTO user_info (member_id, {column}) VALUES ($1, $2)
                     ON CONFLICT (member_id) DO UPDATE SET {column} = $2;''')
-        await self.db_conn.execute(execute, member_id, col_value)
+        await self.pg_con.execute(execute, member_id, col_value)
