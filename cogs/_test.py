f6baeeb32dd34d47a17f2225786025c5993b9200
James <naughtsee@gmail.com>
2017-09-24 00:58:44 -0400

    d.py bug test

---
+++ cogs/_test.py
@@ -0,0 +1,24 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+
+class test:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def testme(self, ctx):
+        guild = ctx.guild
+
+        # Move to specific category
+        temp_category = guild.get_channel(361375380042416129)
+
+        if temp_category:
+            channel = await guild.create_voice_channel('testing for memes')
+
+            await channel.edit(category=temp_category)
+
+
+def setup(bot):
+    bot.add_cog(test(bot))
