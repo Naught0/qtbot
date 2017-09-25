5ea372fdd7bce8eeff85d3c9d4675efd566b342e
James <naughtsee@gmail.com>
2017-09-25 03:11:37 -0400

    try to bust the cache

--- cogs/ip.py
+++ cogs/ip.py
@@ -3,6 +3,7 @@
 import discord
 from discord.ext import commands
 from utils import aiohttp_wrap as aw
+from datetime import datetime
 
 class IPLookup:
     def __init__(self, bot):
@@ -22,10 +23,10 @@ class IPLookup:
         em = discord.Embed(title=res['org'], color=discord.Color.dark_magenta())
         em.add_field(name='Location', value=f"{res['city']}, {res['regionName']}, {res['country']}", inline=False)
         em.add_field(name='Coordinates', value=f"({res['lat']:.3f}, {res['lon']:.3f})")
-        em.add_field(name='ISP', value=res['isp'])
+        em.add_field(name='ISP', value=res['as'])
         em.set_thumbnail(url='http://www.iconsdb.com/icons/preview/white/wifi-xxl.png')
         em.set_image(
-            url=f"https://maps.googleapis.com/maps/api/staticmap?center={res['lat']},{res['lon']}&zoom=13&scale=false&size=255x255&maptype=roadmap&format=png&markers=size:mid%7Ccolor:0xAD1457%7Clabel:%7C{res['lat']},{res['lon']}")
+            url=f"https://maps.googleapis.com/maps/api/staticmap?center={res['lat']},{res['lon']}&zoom=13&scale=false&size=255x255&maptype=roadmap&format=png&markers=size:mid%7Ccolor:0xAD1457%7Clabel:%7C{res['lat']},{res['lon']}#{str(datetime.datetime.now())}")
 
         await ctx.send(embed=em)
 
