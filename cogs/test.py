3880cf9b070f1047158a89841a8698124276d482
James <naughtsee@gmail.com>
2017-09-26 01:47:20 -0400

    remove unecessary stuff for now

--- cogs/test.py
+++ cogs/test.py
@@ -36,38 +36,38 @@ class Admin:
             return f'```py\n{e.__class__.__name__}: {e}\n```'
         return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
-    @commands.command(hidden=True)
-    async def load(self, ctx, *, module):
-        """Loads a module."""
-        try:
-            self.bot.load_extension(module)
-        except Exception as e:
-            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
-        else:
-            await ctx.send('\N{OK HAND SIGN}')
+    # @commands.command(hidden=True)
+    # async def load(self, ctx, *, module):
+    #     """Loads a module."""
+    #     try:
+    #         self.bot.load_extension(module)
+    #     except Exception as e:
+    #         await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+    #     else:
+    #         await ctx.send('\N{OK HAND SIGN}')
+    #
+    # @commands.command(hidden=True)
+    # async def unload(self, ctx, *, module):
+    #     """Unloads a module."""
+    #     try:
+    #         self.bot.unload_extension(module)
+    #     except Exception as e:
+    #         await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+    #     else:
+    #         await ctx.send('\N{OK HAND SIGN}')
+    #
+    # @commands.command(name='reload', hidden=True)
+    # async def _reload(self, ctx, *, module):
+    #     """Reloads a module."""
+    #     try:
+    #         self.bot.unload_extension(module)
+    #         self.bot.load_extension(module)
+    #     except Exception as e:
+    #         await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+    #     else:
+    #         await ctx.send('\N{OK HAND SIGN}')
 
-    @commands.command(hidden=True)
-    async def unload(self, ctx, *, module):
-        """Unloads a module."""
-        try:
-            self.bot.unload_extension(module)
-        except Exception as e:
-            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
-        else:
-            await ctx.send('\N{OK HAND SIGN}')
-
-    @commands.command(name='reload', hidden=True)
-    async def _reload(self, ctx, *, module):
-        """Reloads a module."""
-        try:
-            self.bot.unload_extension(module)
-            self.bot.load_extension(module)
-        except Exception as e:
-            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
-        else:
-            await ctx.send('\N{OK HAND SIGN}')
-
-    @commands.command(pass_context=True, hidden=True, name='eval')
+    @commands.command(pass_context=True, hidden=True, name='pyval')
     async def _eval(self, ctx, *, body: str):
         """Evaluates a code"""
 
@@ -204,48 +204,48 @@ class Admin:
                 await ctx.send(f'Unexpected error: `{e}`')
 
 
-    @commands.command(hidden=True)
-    async def sql(self, ctx, *, query: str):
-        """Run some SQL."""
-        # the imports are here because I imagine some people would want to use
-        # this cog as a base for their other cog, and since this one is kinda
-        # odd and unnecessary for most people, I will make it easy to remove
-        # for those people.
-        from .utils.formats import TabularData, Plural
-        import time
-
-        query = self.cleanup_code(query)
-
-        is_multistatement = query.count(';') > 1
-        if is_multistatement:
-            # fetch does not support multiple statements
-            strategy = ctx.db.execute
-        else:
-            strategy = ctx.db.fetch
-
-        try:
-            start = time.perf_counter()
-            results = await strategy(query)
-            dt = (time.perf_counter() - start) * 1000.0
-        except Exception:
-            return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
-
-        rows = len(results)
-        if is_multistatement or rows == 0:
-            return await ctx.send(f'`{dt:.2f}ms: {results}`')
-
-        headers = list(results[0].keys())
-        table = TabularData()
-        table.set_columns(headers)
-        table.add_rows(list(r.values()) for r in results)
-        render = table.render()
-
-        fmt = f'```\n{render}\n```\n*Returned {Plural(row=rows)} in {dt:.2f}ms*'
-        if len(fmt) > 2000:
-            fp = io.BytesIO(fmt.encode('utf-8'))
-            await ctx.send('Too many results...', file=discord.File(fp, 'results.txt'))
-        else:
-            await ctx.send(fmt)
+    # @commands.command(hidden=True)
+    # async def sql(self, ctx, *, query: str):
+    #     """Run some SQL."""
+    #     # the imports are here because I imagine some people would want to use
+    #     # this cog as a base for their other cog, and since this one is kinda
+    #     # odd and unnecessary for most people, I will make it easy to remove
+    #     # for those people.
+    #     from .utils.formats import TabularData, Plural
+    #     import time
+    #
+    #     query = self.cleanup_code(query)
+    #
+    #     is_multistatement = query.count(';') > 1
+    #     if is_multistatement:
+    #         # fetch does not support multiple statements
+    #         strategy = ctx.db.execute
+    #     else:
+    #         strategy = ctx.db.fetch
+    #
+    #     try:
+    #         start = time.perf_counter()
+    #         results = await strategy(query)
+    #         dt = (time.perf_counter() - start) * 1000.0
+    #     except Exception:
+    #         return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+    #
+    #     rows = len(results)
+    #     if is_multistatement or rows == 0:
+    #         return await ctx.send(f'`{dt:.2f}ms: {results}`')
+    #
+    #     headers = list(results[0].keys())
+    #     table = TabularData()
+    #     table.set_columns(headers)
+    #     table.add_rows(list(r.values()) for r in results)
+    #     render = table.render()
+    #
+    #     fmt = f'```\n{render}\n```\n*Returned {Plural(row=rows)} in {dt:.2f}ms*'
+    #     if len(fmt) > 2000:
+    #         fp = io.BytesIO(fmt.encode('utf-8'))
+    #         await ctx.send('Too many results...', file=discord.File(fp, 'results.txt'))
+    #     else:
+    #         await ctx.send(fmt)
 
 def setup(bot):
     bot.add_cog(Admin(bot))
