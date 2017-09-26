a80e415fa20ab1c4723a15a31e275c3ce7e0f4b4
James <naughtsee@gmail.com>
2017-09-26 01:41:07 -0400

    missed a portion

--- cogs/eval.py
+++ cogs/eval.py
@@ -13,6 +13,15 @@ class Eval:
         self.bot = bot
         self.db_conn = bot.pg_con
 
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
     @commands.command(name='eval', hidden=True)
     @commands.is_owner()
     async def shell_access(self, ctx, *, cmd):
