d43b7eec7f38be84f717fa5458ae9a5be1fc7198
James <naughtsee@gmail.com>
2017-10-08 00:41:41 -0400

    create my own async giphy wrapper

--- cogs/gif.py
+++ cogs/gif.py
@@ -1,44 +1,31 @@
 import discord
-import giphypop
+from utils import giphy_wrapper as gwrap
 from discord.ext import commands
 
 
 class Giphy:
     def __init__(self, bot):
         self.bot = bot
+        self.aio_session = bot.aio_session
 
-    def sync_giphy(query=None):
-        """ Non async giphy library function """
-        giphy_instance = giphypop.Giphy()
-
-        if query is None:
-            return giphy_instance.random_gif()
-
-        gif = giphy_instance.random_gif(query)
-
-        if gif:
-            return gif
-
-        return gif
 
     @commands.command(name='gif', aliases=['jif', 'jiff'])
     async def giphy(self, ctx, *, query=None):
         """ returns a random gif matching a query """
-        gif_result = await self.bot.loop.run_in_executor(None, Giphy.sync_giphy, query)
+        gif_result = await gwrap.rand_search(self.aio_session, query=query)
 
         if gif_result:
-            await ctx.send(gif_result.media_url)
+            await ctx.send(gif_result['url'])
         else:
             await ctx.send(f"Sorry, I couldn't find anything for `{query}`.")
 
-    # Thanks qtbot!
     @commands.command(aliases=['ty', 'thank'])
+    @commands.cooldown()
     async def thanks(self, ctx):
         """ Thank your overlord, qtbot """
-        gif_result = await self.bot.loop.run_in_executor(None, Giphy.sync_giphy, 'blushing')
+        gif_result = await gwrap.rand_search(self.aio_session, query='blushing')
 
-        await ctx.send(gif_result.fixed_width.downsampled.url)
-
+        await ctx.send(gif_result['fixed_width_downsampled_url'])
 
 def setup(bot):
     bot.add_cog(Giphy(bot))
---
+++ utils/giphy_wrapper.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+
+import re
+import utils.aiohttp_wrap as aw
+
+async def rand_search(aiohttp_session, query: str="") -> dict:
+    if query:
+        stripped_query = re.sub('[^A-Za-z0-9 ]+', '', query).replace(' ', '%20')
+        api_url = f'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag={stripped_query}'
+    else:
+        api_url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
+
+    response = await aw.aio_get_json(aiohttp_session, api_url)
+    return response['data']
