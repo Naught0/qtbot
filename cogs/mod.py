332db5d13b5c53c70bd48cff6c6b486343c92dde
James <naughtsee@gmail.com>
2017-10-08 20:22:49 -0400

    add proper permissions for kicking / banning

--- cogs/mod.py
+++ cogs/mod.py
@@ -9,12 +9,13 @@ class Moderator:
 
     async def on_command_error(self, ctx, error):
         if isinstance(error, discord.Forbidden):
-            return await ctx.send('Sorry, insufficient privileges.')
+            return await ctx.send('Sorry, I do not have sufficient privileges to do that.')
 
         if isinstance(error, discord.HTTPException):
             return await ctx.send('Sorry, something went wrong with the connection. Try again or do this manually.')
 
     @commands.command(aliases=['k'])
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user: discord.User, *, reason=None):
         """ Kick a user from the server """
         await ctx.guild.kick(user, reason=reason)
@@ -22,6 +23,7 @@ class Moderator:
                        f'Reason: `{reason}`.')
 
     @commands.command(aliases=['kb'])
+    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, user: discord.User, *, reason=None):
         """ Ban a user from the server """
         await ctx.guild.ban(user, reason=reason, delete_message_days=0)
@@ -29,6 +31,7 @@ class Moderator:
                        f'Reason: `{reason}`.')
 
     @commands.command(aliases=['ub'])
+    @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, user: discord.User, *, reason=None):
         """ Unban a user from the server """
         await ctx.guild.unban(user, reason=reason)
