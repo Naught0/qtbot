f228395ed2d08af328c0b11e9882e421a65c5bb7
James <naughtsee@gmail.com>
2017-10-08 20:38:10 -0400

    move error handling to error.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -7,13 +7,6 @@ class Moderator:
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, discord.Forbidden):
-            return await ctx.send('Sorry, I do not have sufficient privileges to do that.')
-
-        if isinstance(error, discord.HTTPException):
-            return await ctx.send('Sorry, something went wrong with the connection. Try again or do this manually.')
-
     @commands.command(aliases=['k'])
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user: discord.User, *, reason=None):
