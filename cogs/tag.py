e7d8d2a32e87d130d6dfcc27baed0089b8d5f63e
James <naughtsee@gmail.com>
2017-10-11 02:57:16 -0400

    missed a char

--- cogs/tag.py
+++ cogs/tag.py
@@ -188,7 +188,7 @@ class Tag:
         top_tagger_record_list = await self.pg_con.fetch(get_top_taggers, ctx.guild.id)
         tt_build_list = []
 
-        for idx, record in enumerate(top_taggerrecord_list):
+        for idx, record in enumerate(top_tagger_record_list):
             tt_build_list.append(f'{self.emoji_map[idx]} <@{record["owner_id"]}> | {record["count"]} tags created')
 
         em.add_field(name='Top Taggers', value='\n'.join(tt_build_list))
