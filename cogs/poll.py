f14ada81e80942663cbed519863595108ea504e0
James <naughtsee@gmail.com>
2017-10-12 22:34:56 -0400

    clean it up

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,16 +1,18 @@
 #!/bin/env python3
+
 import discord
 import asyncio
 from datetime import datetime
+from functools import reduce
 from utils import dict_manip as dm
 from discord.ext import commands
 
 class Poll:
     def __init__(self, bot):
         self.bot = bot
-        self.emoji_map = ['1\U000020e3', '2\U000020e3', '3\U000020e3', '4\U000020e3', 
+        self.emoji_tup = ('1\U000020e3', '2\U000020e3', '3\U000020e3', '4\U000020e3',
                           '5\U000020e3', '6\U000020e3', '7\U000020e3', '8\U000020e3', 
-                          '9\U000020e3']
+                          '9\U000020e3')
 
     @commands.command(aliases=['survey'])
     async def poll(self, ctx, poll_name: str, timeout: float, *, poll_options: str):
@@ -24,11 +26,15 @@ class Poll:
 
         option_list = poll_options.split(',')
 
+        # Gotta have options
         if len(option_list) < 2:
             return await ctx.send('You need at least 2 options to call it a poll, jeez.')
-        elif len(option_list) > len(self.emoji_map):
+
+        # 9 is options thanks
+        elif len(option_list) > len(self.emoji_tup):
             return await ctx.send("That's too many options ")
 
+        # Poll name will cause an error if above this value
         if len(poll_name) > 256:
             return await ctx.send('Sorry, titles must be under 256 characters per Discord limits.')
 
@@ -38,38 +44,43 @@ class Poll:
         em.set_footer(text='Poll created at')
         em.timestamp = datetime.now()
 
-        description_list = []
-        poll_votes = {}
+        poll_results = {}
         for idx, opt in enumerate(option_list):
-            description_list.append(f'{self.emoji_map[idx]} {opt}')
-            poll_votes[f'{self.emoji_map[idx]}'] = 0
+            # Set votes to -1 so as not to count bot reactions
+            poll_results[self.emoji_tup[idx]] = {'opt_text': f'{self.emoji_tup[idx]} {opt}', 'votes': -1}
 
-        em.description = '\n'.join(description_list)
+        em.description = '\n'.join([x['opt_text'] for x in poll_results.values()])
 
+        # Save message obj to a variable
         poll_msg = await ctx.send(embed=em)
 
-        for e in self.emoji_map[:len(option_list)]:
+        # Add as many reactions as options
+        for e in poll_results:
             await poll_msg.add_reaction(e)
 
+        # Checks whether user has already voted & whether their emoji is allowed
         def check(reaction, user):
-            return user not in user_set and reaction.emoji in poll_votes
+            return user not in user_set and reaction.emoji in poll_results
 
+        # User set because hashes are freaky-fast
+        # Thanks Beat (:
         user_set = set()
 
         while True:
-            try: 
+            try:
                 reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=timeout)
             except asyncio.TimeoutError:
                 await poll_msg.clear_reactions()
                 break
             
-            if reaction.emoji in poll_votes:
+            if reaction.emoji in poll_results:
                 user_set.add(user)
-                poll_votes[reaction.emoji] += 1
+                poll_results[reaction.emoji]['votes'] += 1
 
-        chan_choice = dm.key_with_max_value(poll_votes)
+        predicate = lambda x, y: x if poll_results[x]['votes'] > poll_results[y]['votes'] else y
+        chan_choice = reduce(predicate, poll_results.keys())
         await ctx.send('The channel has spoken!\n'
-                       f'The best option is {chan_choice} with {poll_votes[chan_choice]} votes.')
+                       f'The best option is {poll_results[chan_choice]["opt_text"]}  with {poll_results[chan_choice]["votes"]} vote(s).')
         
 
 def setup(bot):
