c8fdb26e3b2e134ebab78c864499996df8f03960
James <naughtsee@gmail.com>
2017-10-16 17:49:17 -0400

    add initial reverse image search cog

---
+++ cogs/ris.py
@@ -0,0 +1,31 @@
+#!/bin/env python3
+
+import discord
+from discord.ext import commands
+
+
+class RIS:
+    def __init__(self, bot):
+        self.bot = bot
+        self.aiohttp_session = bot.aio_session
+        self.gyaku_url = 'localhost:8000/search'
+
+    @commands.command()
+    async def reverse_image_search(self, ctx, *, url: str):
+        """ Do a google reverse image search """
+        async with self.aiohttp_session.post(self.gyaku_url, data=url) as r:
+            resp_data = await r.json()
+
+        if resp_data['error'] is not None:
+            return await ctx.send(f'Sorry, I could not find anything for that one.\n```{resp_data["error"]}```')
+
+        em = discord.Embed(title=':frame_photo: Image Search Results', color=discord.Color.dark_blue())
+
+        if resp_data['best_guess']:
+            em.add_field(name='Best guess', value=resp_data['best_guess'])
+
+        await ctx.send(embed=em)
+
+
+def setup(bot):
+    bot.add_cog(RIS(bot))
