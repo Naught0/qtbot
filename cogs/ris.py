d5e1ec5997f8194f70545864d6fa058278576d51
James <naughtsee@gmail.com>
2017-10-16 17:55:13 -0400

    debug sent to chat

--- cogs/ris.py
+++ cogs/ris.py
@@ -14,17 +14,18 @@ class RIS:
     async def reverse_image_search(self, ctx, *, url: str):
         """ Do a google reverse image search """
         async with self.aiohttp_session.post(self.gyaku_url, data=url) as r:
-            resp_data = await r.json()
+            resp_data = await r.text()
 
-        if resp_data['error'] is not None:
-            return await ctx.send(f'Sorry, I could not find anything for that one.\n```{resp_data["error"]}```')
-
-        em = discord.Embed(title=':frame_photo: Image Search Results', color=discord.Color.dark_blue())
-
-        if resp_data['best_guess']:
-            em.add_field(name='Best guess', value=resp_data['best_guess'])
-
-        await ctx.send(embed=em)
+        await ctx.send(f'```{resp_data}```')
+        # if resp_data['error'] is not None:
+        #     return await ctx.send(f'Sorry, I could not find anything for that one.\n```{resp_data["error"]}```')
+        #
+        # em = discord.Embed(title=':frame_photo: Image Search Results', color=discord.Color.dark_blue())
+        #
+        # if resp_data['best_guess']:
+        #     em.add_field(name='Best guess', value=resp_data['best_guess'])
+        #
+        # await ctx.send(embed=em)
 
 
 def setup(bot):
