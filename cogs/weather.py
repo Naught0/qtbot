a4438be327959f8dd02490dc97ac94b6991cbf8b
James <naught0@github.com>
2017-12-01 13:25:44 -0500

    reformatting & attempt to allow non-us zipcodes

--- cogs/weather.py
+++ cogs/weather.py
@@ -18,6 +18,7 @@ class Weather:
                                 'kelvin': {'temp': 'K', 'dt': 'm/s'}}
         self.owm_url = 'http://api.openweathermap.org/data/2.5/weather'
         self.wunder_url = 'http://api.wunderground.com/api/{}/forecast/geolookup/conditions/q/{}.json'
+        self.cache_ttl = 3600
 
         with open('data/apikeys.json') as fp:
             api_keys = json.load(fp)
@@ -26,9 +27,9 @@ class Weather:
         self.api_key = api_keys['open_weather']
 
     @commands.command(name='az')
-    async def add_zip(self, ctx, zip_code):
+    async def add_zip(self, ctx, zip_code, region='us'):
         """ Add your zipcode to qtbot's database so you don't have to supply it later """
-        await self.db.insert_user_info(ctx.author.id, 'zipcode', zip_code)
+        await self.db.insert_user_info(ctx.author.id, 'zipcode', f'{zip_code},{region}')
         await ctx.send(f'Successfully added zipcode `{zip_code}`.')
 
     @commands.command(name='rz')
@@ -38,8 +39,8 @@ class Weather:
         await ctx.send(f'Successfully removed zipcode for `{str(ctx.author)}`.')
 
     @commands.command(name='wt')
-    async def get_weather(self, ctx, zip_code='', region_abv='us'):
-        """ Get the weather via zipcode """
+    async def get_weather(self, ctx, zip_code='', region='us'):
+        """ Get the weather via zipcode, default region US"""
         if not zip_code:
             zip_code = await self.db.fetch_user_info(ctx.author.id, 'zipcode')
 
@@ -48,34 +49,38 @@ class Weather:
             return await ctx.send(
                 "Sorry, you're not in my file. Please use `az` to add your zipcode, or supply one to the command.")
 
+        # This is the format the OWM expects. US is the default.
+        formatted_zip = f'{zip_code},{region}'
+
         # Get the OWM units param for the call if one is to be made
         # This is also used later to determine °F or °C and m/s vs mph
-        units = 'imperial' if region_abv == 'us' else 'metric'
+        units = 'imperial' if region.lower() in ['us', 'usa'] else 'metric'
 
+        redis_key = f'{zip_code}:{units}:weather'
         # Check for cached results in redis server
-        if await self.redis_client.exists(f'{zip_code}:weather'):
-            owm_data = await self.redis_client.get(f'{zip_code}:weather')
+        if await self.redis_client.exists(redis_key):
+            owm_data = await self.redis_client.get(redis_key)
             owm_data = json.loads(owm_data)
 
-        # Store reults in cache for 7200 seconds
-        # This is the frequency with which the API updates so there's no use in querying at a faster rate
+        # Store reults in cache
+        # The API updates every 7200 seconds
         else:
-            params = {'zip': zip_code, 'APPID': self.api_key, 'units': units}
+            params = {'zip': formatted_zip, 'APPID': self.api_key, 'units': units}
             owm_data = await aw.aio_get_json(self.aio_session, self.owm_url, params=params)
 
             if not owm_data:
                 return await ctx.send(f"Sorry, I couldn't find weather for `{zip_code}`.")
 
-            await self.redis_client.set(f'{zip_code}:weather', json.dumps(owm_data), ex=7200)
+            await self.redis_client.set(redis_key, json.dumps(owm_data), ex=self.cache_ttl)
 
         # Create the embed
         em = discord.Embed(title=f"Weather for {owm_data['name']}", color=discord.Color.dark_orange())
-
         em.add_field(name='Temperature', 
                      value=f"{float(owm_data['main']['temp']):.2f}{self.locale_abv_dict[units]['temp']}") 
         em.add_field(name='Conditions', value=f"{owm_data['weather'][0]['description'].capitalize()}")
         em.add_field(name='Humidity', value=f"{owm_data['main']['humidity']}%")
-        em.add_field(name='Winds', value=f"{float(owm_data['wind']['speed']):.2f} {self.locale_abv_dict[units]['dt']}")
+        em.add_field(name='Winds', value=f"{float(owm_data['wind']['speed']):.2f}" 
+                                         f"{self.locale_abv_dict[units]['dt']}")
         em.set_thumbnail(url=f"http://openweathermap.org/img/w/{owm_data['weather'][0]['icon']}.png")
 
         await ctx.send(embed=em)
@@ -95,8 +100,7 @@ class Weather:
             forecast_data = await self.redis_client.get(f'{zip_code}:forecast')
             forecast_data = json.loads(forecast_data)
 
-        # Store reults in cache for 7200 seconds
-        # This is the frequency with which the API updates so there's no use in querying at a faster rate
+        # Store reults in cache 
         else:
             resp = await aw.aio_get_json(self.aio_session, self.wunder_url.format(self.wunder_api_key, zip_code))
 
@@ -106,7 +110,7 @@ class Weather:
 
             # Compresses the call into something more managable and less wasteful than the whole mess of json it gives
             forecast_data = resp['forecast']['txt_forecast']['forecastday']
-            await self.redis_client.set(f'{zip_code}:forecast', json.dumps(forecast_data), ex=7200)
+            await self.redis_client.set(f'{zip_code}:forecast', json.dumps(forecast_data), ex=self.cache_ttl)
 
         await ctx.send(
             f"Tomorrow: `{forecast_data[2]['fcttext']}`\nTomorrow Evening: `{forecast_data[3]['fcttext']}`")
