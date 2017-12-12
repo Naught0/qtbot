0d030363d05b06bd7553b2c914514a5940175c91
James <naught0@github.com>
2017-12-12 17:24:57 -0500

    better aiohttp wrapper approach impl for testing

--- utils/aiohttp_wrap.py
+++ utils/aiohttp_wrap.py
@@ -1,19 +1,22 @@
 import aiohttp
 
 
-SUCCESS = 200
-
-async def aio_get_text(aio_session, url, headers=None, params=None):
-    async with aio_session.get(url, headers=headers, params=params) as r:
-        if r.status == SUCCESS:
+async def aio_get_text(session: aiohttp.ClientSession, url: str, headers: dict = None, params: dict = None):
+    async with session.get(url, headers=headers, params=params) as r:
+        if r.status == 200:
             return await r.text()
         else:
             return None
 
 
-async def aio_get_json(aio_session, url, headers=None, params=None):
-    async with aio_session.get(url, headers=headers, params=params) as r:
-        if r.status == SUCCESS:
+async def aio_get_json(session, url, headers=None, params=None):
+    async with session.get(url, headers=headers, params=params) as r:
+        if r.status == 200:
             return await r.json()
         else:
             return None
+
+async def session_get(session: aiohttp.ClientSession, *args, **kwargs):
+    """ A revamped helper function to reduce aiohttp boilerplate """
+    async with session.get(*args, **kwargs) as r:
+        return r
