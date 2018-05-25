bf810e32b05192b3f3517abb4dcf52dcf09c6c0e
James <naughtsee@gmail.com>
2018-05-25 19:19:00 -0400

    item data manually updated due to rsbuddy api 503s

--- cogs/osrs.py
+++ cogs/osrs.py
@@ -61,19 +61,33 @@ class OSRS:
     @commands.is_owner()
     async def _update(self, ctx):
         """A command to update the OSRS GE item list"""
-        new_items = await aw.aio_get_json(self.aio_session, self.items_uri)
-        if len(new_items) == len(self.item_data):
-            em = discord.Embed(title=':no_entry_sign: Items already up-to-date boss!',
-                               color=discord.Color.dark_red())
+        # new_items = await aw.aio_get_json(self.aio_session, self.items_uri)
+        #
+        # # This 503's a lot, if not every time, not sure yet
+        # if new_items is None:
+        #     em = discord.Embed(title=':no_entry_sign: RS buddy is serving up a 503!',
+        #                        color=discord.Color.dark_red())
+        #
+        #     return await ctx.send(embed=em)
+        #
+        # if len(new_items) == len(self.item_data):
+        #     em = discord.Embed(title=':no_entry_sign: Items already up-to-date boss!',
+        #                        color=discord.Color.dark_red())
+        #
+        #     return await ctx.send(embed=em)
+        #
+        # with open('data/item-data.json', 'w') as f:
+        #     json.dump(new_items, f, indent=2)
+        #
+        # num_updated = len(new_items) - len(self.item_data)
+        # em = discord.Embed(title=f':white_check_mark: {num_updated} Item(s) updated!',
+        #                    color=discord.Color.dark_green())
 
-            return await ctx.send(embed=em)
-
-        with open('data/item-data.json', 'w') as f:
-            json.dump(new_items, f, indent=2)
-
-        num_updated = len(new_items) - len(self.item_data)
-        em = discord.Embed(title=f':white_check_mark: {num_updated} Item(s) updated!',
-                           color=discord.Color.dark_green())
+        # The osbuddy api just 503s every time, keeping this up
+        # in the hopes that this works in the future
+        em = discord.Embed(title=':white_check_mark: Check here',
+                           url='https://rsbuddy.com/exchange/names.json',
+                           color=discord.Color.dark_gold())
 
         await ctx.send(embed=em)
 
