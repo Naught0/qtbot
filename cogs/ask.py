dc9a1767ccedf993f8baae43950ee2b46b81f0cc
James <naughtsee@gmail.com>
2018-07-03 19:18:03 -0400

    clean up code a bit

--- cogs/ask.py
+++ cogs/ask.py
@@ -1,26 +1,26 @@
 import ast
 import asyncio
-import discord
-
 from typing import List
 
-from lxml import etree
-from discord.ext import commands
+import discord
 from bs4 import BeautifulSoup
+from discord.ext import commands
+from lxml import etree
+
 from utils import aiohttp_wrap as aw
 
 
 class Google:
     BING_URI = 'https://wwww.bing.com/images/search'
     BING_H = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/4.0; GTB7.4; '
-                                 'InfoPath.1; SV1; .NET CLR 2.8.52393; WOW64; en-US)'}
-    EMOJIS = [f'{x}\U000020e3' for x in range(1,10)]
+                            'InfoPath.1; SV1; .NET CLR 2.8.52393; WOW64; en-US)'}
+    EMOJIS = [f'{x}\U000020e3' for x in range(1, 10)]
 
     def __init__(self, bot):
         self.bot = bot
         self.aio_session = bot.aio_session
         self.redis_client = bot.redis_client
-        self.scrape_uri = 'http://www.ask.com/web?q={}&o=0&qo=homepageSearchBox'
+        self.scrape_uri = 'http://www.ask.com/web?o=0&qo=homepageSearchBox'
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
                                       'Chrome/41.0.2228.0 Safari/537.36'}
 
@@ -37,29 +37,27 @@ class Google:
 
         return link_list or None
 
-    @commands.group(invoke_without_command=True, name='ask', aliases=['g'])
-    async def ask_search(self, ctx, *query):
+    @commands.group(invoke_without_command=True, name='google', aliases=['g', 'ask'])
+    async def _google(self, ctx, *, query):
         """ Get search results from [REDACTED], now that Google hates me. """
 
         # Handle no input
         if not query:
             return await ctx.send('Feel free to search something.')
 
-        # Format query for search url
-        search_query = '+'.join(query)
-
         # Check if this has been searched in the cache
-        if await self.redis_client.exists(f'ask:{search_query}'):
-            link_list_str = await self.redis_client.get(f'ask:{search_query}')
+        if await self.redis_client.exists(f'ask:{query}'):
+            link_list_str = await self.redis_client.get(f'ask:{query}')
             link_list = ast.literal_eval(link_list_str)
 
         # Actually request page html and store the link list for 6 hrs
         else:
-            resp_html = await aw.aio_get_text(self.aio_session, self.scrape_uri.format(search_query), headers=self.headers)
+            resp_html = await aw.aio_get_text(self.aio_session, self.scrape_uri, params={'q': query},
+                                              headers=self.headers)
             link_list = self._get_ask_links(resp_html)
 
             if link_list:
-                await self.redis_client.set(f'ask:{search_query}', f'{link_list}', ex=21600)
+                await self.redis_client.set(f'ask:{query}', f'{link_list}', ex=21600)
             else:
                 return await ctx.error(f"Sorry, I couldn't find anything for `{query}`.")
 
@@ -83,7 +81,7 @@ class Google:
 
         return em_dict
 
-    @ask_search.command(name='images', aliases=['-i'])
+    @_google.command(name='images', aliases=['-i', 'image'])
     async def bing_image_search(self, ctx, *, query):
         """Search for some fantastic imagery my guy"""
         # No input
