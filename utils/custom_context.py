f52a0f23087c5b7752e122f8460ef38b82d9ac90
James <naughtsee@gmail.com>
2018-07-03 21:35:19 -0400

    alter custom context to allow for descriptions

--- utils/custom_context.py
+++ utils/custom_context.py
@@ -3,14 +3,16 @@ from discord.ext import commands
 
 
 class CustomContext(commands.Context):
-    async def error(self, message: str):
-        em = discord.Embed(title=f':no_entry_sign: {message}',
-                           color=discord.Color.dark_red())
+    async def error(self, title: str, description: str = None):
+        em = discord.Embed(title=f':no_entry_sign: {title}',
+                           color=discord.Color.dark_red(),
+                           description=description)
 
         await self.send(embed=em)
 
-    async def success(self, message: str):
-        em = discord.Embed(title=f':white_check_mark: {message}',
-                           color=discord.Color.dark_green())
+    async def success(self, title: str, description: str = None):
+        em = discord.Embed(title=f':white_check_mark: {title}',
+                           color=discord.Color.dark_green(),
+                           description=None)
 
         await self.send(embed=em)
