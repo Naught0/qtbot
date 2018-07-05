15b9ad8cb014b1430dd1ced1454302458b5d7764
James <naughtsee@gmail.com>
2018-07-05 14:54:34 -0400

    rework error messages

--- cogs/error.py
+++ cogs/error.py
@@ -1,5 +1,6 @@
 import sys
 import traceback
+
 from discord.ext import commands
 
 
@@ -14,22 +15,25 @@ class ErrorHandler:
             return
 
         if isinstance(error, commands.NotOwner):
-            return await ctx.send('Sorry, only the owner of qtbot may run this command.')
+            return await ctx.error('Error', description='Sorry, only the owner of qtbot may run this command.')
 
         if isinstance(error, commands.CommandOnCooldown):
-            return await ctx.send(f'This command is on cooldown. Please retry in `{error.retry_after:.0f}` second(s).')
+            return await ctx.error('Error',
+                                   description='This command is on cooldown. '
+                                               f'Please retry in `{error.retry_after:.0f}` second(s).')
 
         if isinstance(error, commands.MissingRequiredArgument):
-            return await ctx.send(f'Command missing required argument `{error.param}`.')
+            return await ctx.error('Error', description=f'You didn\'t include `{error.param}`.')
 
         if isinstance(error, commands.MissingPermissions):
-            return await ctx.send(f'Sorry you need permissions: `{",".join(error.missing_perms)}` to do that.')
+            return await ctx.error('Error',
+                                   description='Sorry you need permissions '
+                                               f'`{",".join(error.missing_perms)}` to do that.')
 
         if isinstance(error, commands.BotMissingPermissions):
-            return await ctx.send(f'Sorry I need permissions: `{",".join(error.missing_perms)}` to do that.')
-
-        if isinstance(error, commands.BadArgument):
-            return await ctx.send(f"Sorry I couldn't do that. Check your arguments.")
+            return await ctx.error('Error',
+                                   description='Sorry I need permissions '
+                                               f'`{", ".join(error.missing_perms)}` to do that.')
 
         print(f'Ignoring exception in command {ctx.command}:', file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
