17a9677753910873e9cf9d8581857bdb8a2f5ff8
James <4589250+Naught0@users.noreply.github.com>
2019-06-18 20:08:07 -0400

    Compensate for changes to Bing

--- cogs/weather.py
+++ cogs/weather.py
@@ -17,10 +17,14 @@ class Weather:
         self.cache_ttl = 3600
         self.url = 'http://bing.com/search'
         # Oh uh, this will make more sense later
-        self.states = {'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS',
-                       'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
-                       'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI',
-                       'WV', 'WY'}
+        self.states = {"Alabama","Alaska","Arizona","Arkansas","California","Colorado",
+                       "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
+                       "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
+                       "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
+                       "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
+                       "North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
+                       "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
+                       "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"}
         # These are some old IE headers that give an easier page to scrape
         self.headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB6.5; SLCC2; '
                                       '.NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0;'
