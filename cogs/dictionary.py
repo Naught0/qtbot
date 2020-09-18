a5f3194ec64853160eddd975fc7bae80ee8e4608
James <4589250+Naught0@users.noreply.github.com>
2020-09-18 16:30:55 -0400

    replace fault wordnik module & redo dictionary embed

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -1,88 +1,83 @@
 import json
+from urllib.parse import quote_plus
+from types import SimpleNamespace
 
 import discord
-from asyncurban import UrbanDictionary, WordNotFoundError
 from discord.ext import commands
-from wordnik import *
+from asyncurban import UrbanDictionary, WordNotFoundError
+
+from utils import aiohttp_wrap as aw
 
 
 class Dictionary(commands.Cog):
+    WORDNIK_URL = "https://api.wordnik.com/v4/word.json/{}"
+
     def __init__(self, bot):
+        self.PARAMS = {
+            "limit": 1,
+            "includeRelated": False,
+            "useCanonical": True,
+            "includeTags": False,
+            "api_key": bot.api_keys["wordnik"],
+        }
         self.bot = bot
         self.urban = UrbanDictionary(loop=bot.loop, session=bot.aio_session)
 
-        # Load API key
-        with open("data/apikeys.json") as f:
-            apiKeys = json.load(f)
-
-        # Init wordnik objects
-        wordnikKey = apiKeys["wordnik"]
-        WordClient = swagger.ApiClient(wordnikKey, "http://api.wordnik.com/v4")
-
     # Returns the most common definition of a word
     @commands.command(name="define", aliases=["d"])
-    async def wordnik_define(self, ctx, *, word):
+    async def _define(self, ctx, *, word: str):
         """ Provides the definition of a word """
-        wordApi = WordApi.WordApi(Dictionary.WordClient)
 
-        parts_of_speech = {
-            "noun": "n.",
-            "verb": "v.",
-            "adjective": "adj.",
-            "adverb": "adv.",
-            "interjection": "interj.",
-            "conjunction": "conj.",
-            "preposition": "prep.",
-            "pronoun": "pron.",
-        }
+        query = quote_plus(word)
+        resp = aw.aio_get_json(self.WORDNIK_URL.format(query), params=self.PARAMS)
+        if resp is None:
+            return await ctx.error(f"Couldn't find anything on `{word}`")
 
-        result = wordApi.getDefinitions(word)
+        result = SimpleNamespace(**resp[0])
 
-        if not result:
-            return await ctx.error("Sorry, couldn't find that one.")
-
-        final_result = result[0]
-
-        for pos in parts_of_speech:
-            if pos in final_result.partOfSpeech.split("-"):
-                word_pos = parts_of_speech[pos]
-                break
-            else:
-                word_pos = final_result.partOfSpeech
-
-        em = discord.Embed(
-            title=f":book: {word.title()} - {word_pos}",
-            description=f"```{final_result.text}```",
+        em = discord.Embed(color=discord.Color.blurple())
+        em.set_author(
+            name=word.title(),
+            url=result.wordnikUrl,
+            icon_url="https://i.imgur.com/9jO7EYk.png",
         )
+        em.description = result.text
+        em.set_footer(text=result.attributionText)
         await ctx.send(embed=em)
 
     @commands.group(
         invoke_without_subcommand=True, name="urban", aliases=["ud", "urband"]
     )
-    async def _urban(self, ctx, *, word):
+    async def _urban(self, ctx, *, word: str):
         """ Consult the world's leading dictionary """
         try:
             result = await self.urban.get_word(word)
         except WordNotFoundError:
-            return await ctx.error(f"Sorry, couldn't find anything on `{word}`.")
+            return await ctx.error(f"Couldn't find anything on `{word}`")
         except ConnectionError:
-            return await ctx.error(f"Sorry, the UrbanDictionary API buggered off.")
+            return await ctx.error(f"Sorry, the UrbanDictionary API died")
 
-        em = discord.Embed(
-            title=f":closed_book: {word.title()}",
-            description=f"```{result.definition}```",
+        em = discord.Embed(color=discord.Color.blurple())
+        em.set_author(
+            name=word.title(),
+            url=result.permalink,
+            icon_url="https://i.imgur.com/nzyqrIj.png",
         )
+        em.description = result.definition
         await ctx.send(embed=em)
 
-    @_urban.command(name="-r")
+    @_urban.command(name="random", aliases=["-r"])
     async def _random(self, ctx):
         """ Get a random word from UrbanDictionary """
         word = await self.urban.get_random()
 
-        em = discord.Embed(
-            title=f":closed_book: {word.title()}",
-            description=f"```{word.definition}```",
+        em = discord.Embed(color=discord.Color.blurple())
+        em.set_author(
+            name=word.title(),
+            url=word.permalink,
+            icon_url="https://i.imgur.com/nzyqrIj.png",
         )
+        em.description = word.definition
         await ctx.send(embed=em)
 
 
