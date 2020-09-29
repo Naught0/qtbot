6d2b73548ef2600f529033dcf6da3e3ed212a5fa
James <4589250+Naught0@users.noreply.github.com>
2020-09-29 14:11:41 -0400

    add all available english news sources

--- cogs/news.py
+++ cogs/news.py
@@ -10,6 +10,90 @@ from discord.ext import commands
 
 
 class News(commands.Cog):
+    SOURCES = (
+        "abc-news",
+        "abc-news-au",
+        "al-jazeera-english",
+        "ars-technica",
+        "associated-press",
+        "australian-financial-review",
+        "axios",
+        "bbc-news",
+        "bbc-sport",
+        "bleacher-report",
+        "bloomberg",
+        "breitbart-news",
+        "business-insider",
+        "business-insider-uk",
+        "buzzfeed",
+        "cbc-news",
+        "cbs-news",
+        "cnn",
+        "crypto-coins-news",
+        "engadget",
+        "entertainment-weekly",
+        "espn",
+        "espn-cric-info",
+        "financial-post",
+        "football-italia",
+        "fortune",
+        "four-four-two",
+        "fox-news",
+        "fox-sports",
+        "google-news",
+        "google-news-au",
+        "google-news-ca",
+        "google-news-in",
+        "google-news-uk",
+        "hacker-news",
+        "ign",
+        "independent",
+        "mashable",
+        "medical-news-today",
+        "msnbc",
+        "mtv-news",
+        "mtv-news-uk",
+        "national-geographic",
+        "national-review",
+        "nbc-news",
+        "news24",
+        "new-scientist",
+        "news-com-au",
+        "newsweek",
+        "new-york-magazine",
+        "next-big-future",
+        "nfl-news",
+        "nhl-news",
+        "politico",
+        "polygon",
+        "recode",
+        "reddit-r-all",
+        "reuters",
+        "rte",
+        "talksport",
+        "techcrunch",
+        "techradar",
+        "the-american-conservative",
+        "the-globe-and-mail",
+        "the-hill",
+        "the-hindu",
+        "the-huffington-post",
+        "the-irish-times",
+        "the-jerusalem-post",
+        "the-lad-bible",
+        "the-next-web",
+        "the-sport-bible",
+        "the-times-of-india",
+        "the-verge",
+        "the-wall-street-journal",
+        "the-washington-post",
+        "the-washington-times",
+        "time",
+        "usa-today",
+        "vice-news",
+        "wired",
+    )
+
     def __init__(self, bot):
         self.bot = bot
         self.redis_client = bot.redis_client
@@ -44,14 +128,18 @@ class News(commands.Cog):
         *,
         query: commands.clean_content(escape_markdown=True) = None,
     ):
-        """ Get the top 5 articles from Google News (http://newsapi.org) (Paginated) """
+        """Get the latest and greatest news or optionally search for some specific news stories"""
 
         # Add Emojis for navigation
         emoji_tup = tuple(f"{x}\U000020e3" for x in range(1, 10))
 
         em_dict = {}
 
-        params = {"q": query, "pageSize": 9, "sources": "google-news"} if query else {"sources": "google-news", "pageSize": 9}
+        params = (
+            {"q": query, "pageSize": 9, "sources": ",".join(self.SOURCES)}
+            if query
+            else {"sources": ",".join(self.SOURCES), "pageSize": 9}
+        )
 
         redis_key = f"news:{query}" if query else "news"
         if await self.redis_client.exists(redis_key):
@@ -67,11 +155,17 @@ class News(commands.Cog):
                 self.aio_session, self.uri, params=params, headers=self.headers
             )
             if api_response is None:
-                return await ctx.error("API error", description="Something went wrong with that request. Try again later.")
+                return await ctx.error(
+                    "API error",
+                    description="Something went wrong with that request. Try again later.",
+                )
 
             article_list = api_response["articles"]
             if len(article_list) == 0:
-                return await ctx.error("No articles found", description=f"Couldn't find any news on `{query}`")
+                return await ctx.error(
+                    "No articles found",
+                    description=f"Couldn't find any news on `{query}`",
+                )
 
             await self.redis_client.set(redis_key, json.dumps(api_response), ex=10 * 60)
 
@@ -80,7 +174,7 @@ class News(commands.Cog):
 
         bot_message = await ctx.send(embed=em_dict[emoji_tup[0]])
 
-        for emoji in emoji_tup[:len(article_list)]:
+        for emoji in emoji_tup[: len(article_list)]:
             await bot_message.add_reaction(emoji)
 
         def check(reaction, user):
