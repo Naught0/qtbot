1b3c9e5f46f48865882f1087ced0ade168233711
James <4589250+Naught0@users.noreply.github.com>
2020-09-29 15:56:28 -0400

    fix formatting and caching

--- cogs/stonks.py
+++ cogs/stonks.py
@@ -8,6 +8,7 @@ from utils.aiohttp_wrap import aio_get_json
 
 class Stonks(commands.Cog):
     URL = "https://finnhub.io/api/v1/quote"
+    TTL = 60 * 15
     def __init__(self, bot):
         self.bot = bot
         self.session = bot.aio_session
@@ -33,14 +34,15 @@ class Stonks(commands.Cog):
             
             if resp['t'] == 0:
                 return await ctx.error("Stock error", description=f"Couldn't find any stock information for `{symbol}`")
-            await self.redis_client.set(redis_key, json.dumps(resp))
+            await self.redis_client.set(redis_key, json.dumps(resp), ex=self.TTL)
         
         em = discord.Embed(color=discord.Color.blurple())
         em.set_author(name=symbol, icon_url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/259/chart-increasing_1f4c8.png")
-        em.add_field(name="Current Price", value=f"${resp['c']}")
-        em.add_field(name="Previous Close", value=f"${resp['pc']}")
-        em.add_field(name="% Change today", value=f"{(resp['c'] - resp['pc'])/resp['pc']:.2%}")
+        em.add_field(name="Current Price", value=f"${resp['c']:.2f}")
+        em.add_field(name="Previous Close", value=f"${resp['pc']:.2f}")
+        em.add_field(name="% Change Today", value=f"{(resp['c'] - resp['pc'])/resp['pc']:.2%}")
 
+        em.set_footer()
         em.timestamp = datetime.fromtimestamp(resp['t'])
 
         await ctx.send(embed=em)
