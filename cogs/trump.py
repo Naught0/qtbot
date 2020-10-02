f1eef8ae59d578b7acb7c2c563e04895f728c792
James <naught0@github.com>
2020-10-01 23:11:33 -0400

    fix emoji

--- cogs/trump.py
+++ cogs/trump.py
@@ -45,7 +45,7 @@ class Trump(commands.Cog):
             resp = await aio_get_json(ctx.bot.aio_session, self.RAND_URL)
         
         em = discord.Embed(color=0x00acee)
-        em.set_author(name="Donald J. Trump :ballot_box_with_check:", icon_url=self.PIC_URL, url=resp["_embedded"]["source"][0]["url"])
+        em.set_author(name="Donald J. Trump ☑️", icon_url=self.PIC_URL, url=resp["_embedded"]["source"][0]["url"])
         em.description = resp["value"]
         em.timestamp = parse(resp["appeared_at"])
         em.set_footer(text="via twitter", icon_url="https://i.imgur.com/DUUkDwY.png")
