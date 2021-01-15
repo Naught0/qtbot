bf4a47521dc62c02a7ac8f4cdbf1a320db9d0d63
James <4589250+Naught0@users.noreply.github.com>
2021-01-15 08:50:54 -0500

    autoformatting & upd8 embed

--- cogs/covid.py
+++ cogs/covid.py
@@ -10,7 +10,7 @@ class Covid(commands.Cog):
     URL = "https://api.covidtracking.com/v1"
 
     @commands.command(name="covid", aliases=["c19", "rona", "corona"])
-    async def _covid(self, ctx, *, state: str=None):
+    async def _covid(self, ctx, *, state: str = None):
         """Get current US covid statistics, optionally passing a 2-letter state code"""
         if state is not None:
             if len(state) != 2:
@@ -19,19 +19,34 @@ class Covid(commands.Cog):
 
         if state is None:
             location = "the US"
-            data = (await aw.aio_get_json(ctx.bot.aio_session, f"{self.URL}/us/current.json"))[0]
+            data = (
+                await aw.aio_get_json(
+                    ctx.bot.aio_session, f"{self.URL}/us/current.json"
+                )
+            )[0]
         else:
             location = state.upper()
-            data = await aw.aio_get_json(ctx.bot.aio_session, f"{self.URL}/states/{state.lower()}/current.json")
+            data = await aw.aio_get_json(
+                ctx.bot.aio_session, f"{self.URL}/states/{state.lower()}/current.json"
+            )
 
         if "error" in data:
             if data["error"]:
                 await ctx.message.add_reaction("❌")
-                return await ctx.send("Something went wrong with the covid API :(", delete_after=10)
-        
+                return await ctx.send(
+                    "Something went wrong with the covid API :(", delete_after=10
+                )
+
         em = discord.Embed()
-        em.title = f"😷 Current Covid statistics in {location}"
-        em.timestamp = isoparse(data["lastModified"] if "lastModified" in data else data["dateModified"])
+        em.color = self.COLOR
+        em.set_author(
+            name=f"😷 Current Covid statistics in {location}",
+            icon_url="https://i.imgur.com/9ka5fCC.png",
+            url=f"https://google.com/search?q=covid+cases+in+{location}",
+        )
+        em.timestamp = isoparse(
+            data["lastModified"] if "lastModified" in data else data["dateModified"]
+        )
 
         em.add_field(name="📈 New cases today", value=f"{data['positiveIncrease']:,}")
         em.add_field(name="☠️ Deaths today", value=f"{data['deathIncrease']:,}")
@@ -43,6 +58,7 @@ class Covid(commands.Cog):
         em.set_footer(text="Last updated")
 
         await ctx.send(embed=em)
+
 
 def setup(bot):
     bot.add_cog(Covid(bot))
