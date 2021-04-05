65293402befa98e380d41a1614f98aacb22e6ecb
James <4589250+Naught0@users.noreply.github.com>
2021-04-05 14:11:16 -0400

    remove debug print statements

--- cogs/ask.py
+++ cogs/ask.py
@@ -69,8 +69,6 @@ class Google(commands.Cog):
             self.link_to_embed(query, x["href"])
             for x in soup.find_all("a", {"class": "thumb"})[:5]
         ]
-        print(soup)
-        print(soup.find_all("a", {"class": "thumb"}))
 
         msg = await ctx.send(embed=embeds[0])
 
