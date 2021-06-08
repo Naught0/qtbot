0c145bf88bb75a789d863e7a763ad3b18fdd7c55
James <4589250+Naught0@users.noreply.github.com>
2021-06-08 10:05:36 -0400

    Move dictionary provided to Merriam Webster

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -1,7 +1,6 @@
 import json
 import re
 from urllib.parse import quote_plus
-from types import SimpleNamespace
 
 import discord
 from discord.ext import commands
@@ -11,54 +10,43 @@ from utils import aiohttp_wrap as aw
 
 
 class Dictionary(commands.Cog):
-    WORDNIK_URL = "https://api.wordnik.com/v4/word.json/{}/definitions"
-    HEADERS = {"Accept": "application/json"}
+    URL = "https://dictionaryapi.com/api/v3/references/collegiate/json/{}"
 
     def __init__(self, bot):
-        self.PARAMS = {
-            "limit": 1,
-            "includeRelated": "false",
-            "useCanonical": "true",
-            "includeTags": "false",
-            "api_key": bot.api_keys["wordnik"],
-            "sourceDictionaries": "ahd,webster,century,wordnet",
-        }
-        self.pattern = re.compile("<.*?>")
-        self.bot = bot
-        self.urban = UrbanDictionary(loop=bot.loop, session=bot.aio_session)
+        self.session = bot.aio_session
+        self.urban = UrbanDictionary(loop=bot.loop, session=self.session)
+        with open(bot.config_file) as f:
+            self.dictionary_key = json.load(f)["webster"]
 
     # Returns the most common definition of a word
-    @commands.command(name="define", aliases=["d"])
+    @commands.command(name="define", aliases=["d", "def", "dictionary", "dict", "dic"])
     async def _define(self, ctx, *, word: str):
-        """ Provides the definition of a word """
-
+        """Provides the definition of a word"""
         query = quote_plus(word)
         resp = await aw.aio_get_json(
-            self.bot.aio_session,
-            self.WORDNIK_URL.format(query),
-            params=self.PARAMS,
-            headers=self.HEADERS,
+            self.session,
+            self.URL.format(query),
+            params={"key": self.dictionary_key},
         )
         if resp is None:
             return await ctx.error(f"Couldn't find anything on `{word}`")
 
-        result = SimpleNamespace(**resp[0])
+        # Grab the first three defn's, combine to string w/ bullet points
+        definitions = "\n".join([f"\u2022 {x}" for x in resp[0]["shortdef"][:3]])
 
         em = discord.Embed(color=discord.Color.blurple())
         em.set_author(
-            name=word.title(),
-            url=result.wordnikUrl,
+            name=f"{word.title()} - _({resp[0]['fl']})_",
+            url=f"https://merriam-webster.com/dictionary/{query}",
             icon_url="https://i.imgur.com/9jO7EYk.png",
         )
-        em.description = self.pattern.sub("", result.text)
-        em.set_footer(text=result.attributionText)
+        em.description = definitions
+        em.set_footer(text=f"Originated ~{re.sub(r'{.+', '', resp[0]['date'])}")
         await ctx.send(embed=em)
 
-    @commands.group(
-        invoke_without_subcommand=True, name="urban", aliases=["ud", "urband"]
-    )
+    @commands.command(name="urban", aliases=["ud", "urband"])
     async def _urban(self, ctx, *, word: str = None):
-        """ Consult the world's leading dictionary or get a random word by searching nothing"""
+        """Consult the world's leading dictionary or get a random word by searching nothing"""
         try:
             if word is None:
                 result = await self.urban.get_random()
