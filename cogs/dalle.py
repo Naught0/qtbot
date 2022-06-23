0be40c549ad233c2b0aae3616d985b95a7b7929a
James <4589250+Naught0@users.noreply.github.com>
2022-06-23 12:26:55 -0400

    Correctly post to dalle API

--- cogs/dalle.py
+++ cogs/dalle.py
@@ -2,6 +2,7 @@ import base64
 import io
 from time import time
 from urllib.parse import urlencode
+import aiohttp
 import discord
 from discord.ext import commands
 from utils import aiohttp_wrap as aw
@@ -11,18 +12,15 @@ class Dalle(commands.Cog):
     @commands.command(aliases=["ai"])
     async def dalle(self, ctx: custom_context.CustomContext, *, prompt: str) -> None:
         async with ctx.typing():
-            count = 0
-            while True:
-                resp = await aw.aio_get_json(ctx.bot.aio_session, "https://backend.craiyon.com/generate", params={"prompt": prompt})
-                count += 1
-                if resp is not None:
-                    break
+            resp: aiohttp.ClientResponse = await ctx.bot.aio_session.post("https://backend.craiyon.com/generate", params={"prompt": prompt})
 
-                if count >= 5:
-                    return await ctx.error("Too much traffic - try again later")
+            if resp.status >= 400:
+                return await ctx.error("Too much traffic - try again later")
+            
+            data = await resp.json()
 
             files = []
-            for pic in resp["images"]:
+            for pic in data["images"]:
                 files.append(discord.File(io.BytesIO(base64.urlsafe_b64decode(pic)), filename=f"{urlencode(prompt)}_{time.time()}.png"))
             
             await ctx.send(prompt, file=files[0])
