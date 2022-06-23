576a25999a05fe7343c01c3bf7a9150752563104
James <4589250+Naught0@users.noreply.github.com>
2022-06-23 13:30:27 -0400

    Stitch images together

--- cogs/dalle.py
+++ cogs/dalle.py
@@ -1,8 +1,10 @@
 import base64
 import io
+from typing import List
 import aiohttp
 import discord
 
+from PIL import Image, ImageOps
 from discord.ext import commands
 from utils import custom_context
 from time import time
@@ -10,22 +12,38 @@ from urllib.parse import quote_plus
 
 class Dalle(commands.Cog):
     @commands.command(aliases=["ai"])
+    @commands.is_owner()
     async def dalle(self, ctx: custom_context.CustomContext, *, prompt: str) -> None:
         async with ctx.typing():
             resp: aiohttp.ClientResponse = await ctx.bot.aio_session.post("https://backend.craiyon.com/generate", json={"prompt": prompt})
 
             if resp.status >= 400:
-                return await ctx.error("Too much traffic - try again later", f"```{resp.status}\n{resp.text}```")
+                return await ctx.error("Too much traffic - try again later", f"```{resp.status}\n{await resp.text()}```")
             
             data = await resp.json()
 
-            files = []
-            for pic in data["images"]:
-                files.append(discord.File(io.BytesIO(base64.urlsafe_b64decode(pic)), filename=f"{quote_plus(prompt)}_{time()}.png"))
-            
-            await ctx.send(prompt, file=files[0])
-        
+            files = [io.BytesIO(base64.urlsafe_b64decode(pic)) for pic in data["images"]]
+            stitched = self.stitch_images(files)
 
+            image = discord.File(stitched, filename=f"{quote_plus(prompt)}_{time()}.png")
+
+        await ctx.send(f"{prompt} {ctx.author.mention}", file=image)
+
+    @staticmethod
+    def stitch_images(images_as_bytes: List[bytes]) -> bytes:
+        width, height = 256, 256
+        images = [Image.open(i) for i in images_as_bytes]
+        images = [ImageOps.fit(i, (256,256), Image.ANTIALIAS) for i in images]
+        shape = (3,3)
+
+        ret = Image.new("RGB", (width * shape[0], height * shape[1]))
+        for row in range(shape[0]):
+            for col in range(shape[1]):
+                offset = width * col, height * row
+                idx = row * shape[1] + col
+                ret.paste(images[idx], offset)
+        
+        return ret.tobytes()
 
 def setup(bot):
     bot.add_cog(Dalle(bot))
