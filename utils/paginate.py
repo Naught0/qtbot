f6371b3a12795b3403b0ac4ae3c2528c42b1936f
James <naught0@users.noreply.github.com>
2022-09-21 15:48:38 -0400

    Add reactions & call values() once

--- utils/paginate.py
+++ utils/paginate.py
@@ -4,9 +4,13 @@ import asyncio
 from typing import List, Tuple
 from discord.ext.commands import Context
 
-EMOJIS = {"back": "⬅️", "forward": "➡️"}
+EMOJI_MAP = {"back": "⬅️", "forward": "➡️"}
 async def paginate(ctx: Context, embeds: List[discord.Embed], timeout=30.0) -> None:
     msg = ctx.message
+    emojis = EMOJI_MAP.values()
+    for reaction in emojis:
+        await msg.add_react(reaction)
+
     current_index = 0
     while True:
         try:
@@ -14,15 +18,15 @@ async def paginate(ctx: Context, embeds: List[discord.Embed], timeout=30.0) -> N
                 "reaction_add",
                 timeout=timeout,
                 check=lambda reaction, user: (
-                    user == ctx.author and reaction.emoji in EMOJIS.values() and reaction.message.id == msg.id
+                    user == ctx.author and reaction.emoji in emojis and reaction.message.id == msg.id
                 ),
             )
         except asyncio.TimeoutError:
             return await msg.clear_reactions()
 
-        if (reaction.emoji == EMOJIS["back"]):
+        if (reaction.emoji == EMOJI_MAP["back"]):
             current_index -= 1
-        if (reaction.emoji == EMOJIS["forward"]):
+        if (reaction.emoji == EMOJI_MAP["forward"]):
             current_index += 1
 
         await msg.edit(embed=embeds[current_index])
