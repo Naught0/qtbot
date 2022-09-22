dd9f5980ded9b10210ea524169ef769a6eff3993
James <naught0@users.noreply.github.com>
2022-09-21 22:45:33 -0400

    Fix pagination logic & typo

--- utils/paginate.py
+++ utils/paginate.py
@@ -1,15 +1,17 @@
 import discord
 import asyncio
 
-from typing import List, Tuple
+from typing import List
 from discord.ext.commands import Context
 
 EMOJI_MAP = {"back": "⬅️", "forward": "➡️"}
+
+
 async def paginate(ctx: Context, embeds: List[discord.Embed], timeout=30.0) -> None:
     msg = ctx.message
     emojis = EMOJI_MAP.values()
-    for reaction in emojis:
-        await msg.add_react(reaction)
+    for emoji in emojis:
+        await msg.add_reaction(emoji)
 
     current_index = 0
     while True:
@@ -24,10 +26,10 @@ async def paginate(ctx: Context, embeds: List[discord.Embed], timeout=30.0) -> N
         except asyncio.TimeoutError:
             return await msg.clear_reactions()
 
-        if (reaction.emoji == EMOJI_MAP["back"]):
-            current_index -= 1
-        if (reaction.emoji == EMOJI_MAP["forward"]):
-            current_index += 1
+        if reaction.emoji == EMOJI_MAP["back"]:
+            current_index = current_index - 1 if current_index > 0 else 0
+        if reaction.emoji == EMOJI_MAP["forward"]:
+            current_index = current_index + 1 if current_index < len(embeds) - 1 else len(embeds) - 1
 
         await msg.edit(embed=embeds[current_index])
         await msg.remove_reaction(reaction.emoji, ctx.author)
