f34c0c3f83941ced094f58ea21e73f384652e0df
James <naught0@users.noreply.github.com>
2022-10-10 15:02:49 -0400

    Increase resolution

--- cogs/diffusion.py
+++ cogs/diffusion.py
@@ -24,8 +24,8 @@ class Diffusion(commands.Cog):
     INPUT = {
         "version": "a9758cbfbd5f3c2094457d996681af52552901775aa2d6dd0b17fd15df959bef",
         "input": {
-            "width": 256,
-            "height": 256,
+            "width": 512,
+            "height": 512,
             "num_outputs": 1,
             "num_outputs": "1",
             "guidance_scale": 7.5,
