737495c2822739d836af8e7eafcd1ed66aeb4995
James <naught0@users.noreply.github.com>
2022-11-17 21:19:44 -0500

    Remove broken nsd command

--- cogs/diffusion.py
+++ cogs/diffusion.py
@@ -22,18 +22,27 @@ class DiffusionError(Exception):
 class Diffusion(commands.Cog):
     URLS = {
         "replicate": "https://inpainter.vercel.app/api/predictions",
-        "happy": "https://healthydiffusion.com/process",
     }
     HEADERS = {"Content-Type": "application/json"}
 
     def __init__(self, bot: QTBot):
         self.bot = bot
 
-    async def image_to_file(self, url: str, prompt: str, spoiler: bool = False) -> File:
+    async def image_to_file(
+        self, url: str, prompt: str, spoiler: bool = False
+    ) -> File:
         image = await (await self.bot.aio_session.get(url)).read()
-        return File(io.BytesIO(image), filename=f"{'SPOILER_' if spoiler else ''}{quote_plus(prompt)}.png")
+        return File(
+            io.BytesIO(image),
+            filename=f"{'SPOILER_' if spoiler else ''}{quote_plus(prompt)}.png",
+        )
 
-    @backoff.on_exception(backoff.expo, ClientResponseError, max_tries=3, giveup=lambda x: x.status == 402)
+    @backoff.on_exception(
+        backoff.expo,
+        ClientResponseError,
+        max_tries=3,
+        giveup=lambda x: x.status == 402,
+    )
     async def req(
         self,
         verb: Literal["GET", "POST"],
@@ -44,7 +53,11 @@ class Diffusion(commands.Cog):
         service: Literal["replicate", "happy"] = "replicate",
     ) -> ClientResponse:
         resp = await self.bot.aio_session.request(
-            verb, f"{self.URLS[service]}{url}", params=params, headers={**headers, **self.HEADERS}, json=data
+            verb,
+            f"{self.URLS[service]}{url}",
+            params=params,
+            headers={**headers, **self.HEADERS},
+            json=data,
         )
 
         resp.raise_for_status()
@@ -65,7 +78,9 @@ class Diffusion(commands.Cog):
         while True:
             if checks >= 45:
                 await self.req("POST", url=f"/{id}/cancel", data={})
-                raise DiffusionError("Couldn't get a result after 90 seconds. Aborting.")
+                raise DiffusionError(
+                    "Couldn't get a result after 90 seconds. Aborting."
+                )
 
             resp = await self.req("GET", url=f"/{id}")
             resp = await resp.json(content_type=None)
@@ -88,45 +103,12 @@ class Diffusion(commands.Cog):
                 return await ctx.error("API Error", f"{ctx.author.mention} {e}")
             except ClientResponseError as e:
                 return await ctx.error(
-                    "API Error", f"{ctx.author.mention} Received status code `{e.status}`\n{e.message}"
+                    "API Error",
+                    f"{ctx.author.mention} Received status code `{e.status}`\n{e.message}",
                 )
 
             file = await self.image_to_file(images[0], prompt)
             return await ctx.send(f"{ctx.author.mention}: {prompt}", file=file)
-
-    async def start_happy_job(self, text: str, unique_id: int) -> None:
-        await self.req("POST", service="happy", data={"text": text, "userID": unique_id})
-
-    async def get_happy_output(self, id: int) -> str:
-        checks = 0
-        while True:
-            if checks >= 45:
-                raise DiffusionError("Couldn't get result after 90 seconds. Aborting.")
-            resp = await self.req("GET", params={"userID": id}, service="happy")
-            data = await resp.json(content_type=None)
-            if data["done"]:
-                return data["url"]
-
-            checks += 1
-            await asyncio.sleep(2)
-
-    @commands.command(aliases=["nsd", "nsfwsd"])
-    async def nsfw_diffusion(self, ctx: CustomContext, *, prompt: str) -> None:
-        is_nsfw: bool = ctx.channel.is_nsfw()
-        async with ctx.typing():
-            try:
-                user_id = randint(0, 10**16)
-                await self.start_happy_job(prompt, user_id)
-                url = await self.get_happy_output(user_id)
-            except DiffusionError as e:
-                return await ctx.error("API Error", f"{ctx.author.mention} {e}")
-            except ClientResponseError as e:
-                return await ctx.error(
-                    "API Error", f"{ctx.author.mention} Received status code `{e.status}`\n{e.message}"
-                )
-
-        file = await self.image_to_file(url, prompt, spoiler=not is_nsfw)
-        return await ctx.send(f"{ctx.author.mention}: {prompt}", file=file)
 
 
 def setup(bot):
