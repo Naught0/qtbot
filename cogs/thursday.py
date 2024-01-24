ccc3cf5393e28e7d9819a015bc3c178a253d70b0
James E <4589250+Naught0@users.noreply.github.com>
2024-01-24 12:19:53 -0500

    Use eastern timezone

--- cogs/thursday.py
+++ cogs/thursday.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 from discord.ext import commands
 from utils.custom_context import CustomContext
@@ -7,7 +8,7 @@ from utils.custom_context import CustomContext
 class Thursday(commands.Cog):
     @commands.command(aliases=["thurs"])
     async def thursday(self, ctx: CustomContext):
-        day = datetime.now().weekday()
+        day = datetime.now(ZoneInfo("US/Eastern")).weekday()
         msg = "<a:alart:1141045442340200610> {} <a:alart:1141045442340200610>"
         if day == 3:
             msg = msg.format("***IT IS CURRENTLY THURSDAY***")
