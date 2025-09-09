b9d81ef05665d28de3a5846d966aac9fe08323e2
justrhoto <otisub@gmail.com>
2025-09-09 12:20:31 -0500

    Add arrows back to the stonk embed

--- cogs/stonks.py
+++ cogs/stonks.py
@@ -193,7 +193,7 @@ class Stonks(commands.Cog):
         )
         em.add_field(
             name="Percent Change",
-            value=f"{percent_change:,.2f}%",
+            value=f"{'⬇️' if '-' in str(percent_change) else '⬆️'} {abs(percent_change):.2f}%",
             inline=False,
         )
         em.add_field(name="Open", value=f"{currency_symbol}{open_:,.2f}")
